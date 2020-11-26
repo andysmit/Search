fb6ad3e5880001a69c5fceec265e72eb9be3c172
Andy <andysmit@uvic.ca>
2020-11-26 12:05:26 -0800

    Added bootstrap and added GET request dummy

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import {Container, Row, Col, Table} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			books: []
+    		};
+		this.getBooks = this.getBooks.bind(this);
+  	}
+
+        componentDidMount(){
+		this.getBooks();
+	}
+
+	getBooks() {
+                var myHeaders = new Headers();
+                myHeaders.append('Content-Type', 'application/json')
+                var requestOptions = {
+                        method: 'GET',
+                        headers: myHeaders
+                }
+                fetch("https://collection.bccampus.ca/wp-json/wp/v2/oer?page=13", requestOptions)
+                        .then(response => response.json())
+                        .then((data) => {
+                                this.setState({books: data})
+				console.log(this.state.books)
+                        })
+                        .catch(error => console.log('error', error));
+		
+		
+	}
+
+	
+	render() {
+		return (
+
+<Container>
+  <Row>
+    <Col>
+      <h1>Duck Data</h1>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <h1>Duck Data</h1>
+		<Table striped bordered hover>
+			<thead>
+				<tr>
+				 <th>link</th>
+				 <th>title</th>
+				 <th>authors</th>
+				 <th>keywords</th>
+				 <th>oertypes</th>
+				 <th>Levels</th>
+				</tr> 
+		   </thead>
+			<tbody>
+			{this.state.books.map((data, key) => {
+				return (
+				<tr>
+					<th>{data.link}</th>
+					<th>{data.title.rendered}</th>
+					<th>{data.authors}</th>
+					<th>{data.keywords}</th>
+					<th>{data.oertype}</th>
+					<th>{data.Levels}</th>
+				</tr>
+				)
+			})}
+			</tbody>
+		</Table>
+    </Col>
+  </Row>
+</Container>
+
+
+  		);
+	}
 }
 
 export default App;
